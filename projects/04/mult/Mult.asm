3e145df44a97e2df094c585a1e97b7b1713dc582
bmitc <65685447+bmitc@users.noreply.github.com>
2020-12-17 21:42:47 -0600

    Annotated Mult.asm with source expressions

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -1,31 +1,32 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Compute R2=R0*R1
-
-// Initialize R2 to 0
-    @R2   // A <- Address[R2]
-    M=0   // R2=0
-
-(LOOP)
-    // Initilize D to R0. We'll use A for the possible jump location.
-    @R0   // A <- Address[R0]
-    D=M   // D=R0
-    // If R0=0, then we are done multiplying
-    @END  // A <- END to prepare for possible jump
-    D;JEQ // If D=0, then jump to END
-    // Otherwise, add R1 to R2 once
-    @R1   // A <- Address[R1]
-    D=M   // D=R1
-    @R2   // A <- Address[R2]
-    M=D+M // R2=R1+R2
-    // Decrement R0 by 1
-    @R0   // A <- Address[R0]
-    M=M-1 // R0=R0-1
-    // Continue looping
-    @LOOP // A <- LOOP
-    0;JMP // Jump to LOOP
-
-(END)
-    @END
-    0;JMP
+// Compute R2=R0*R1                                  --> comment
+                                                  // --> empty
+// Initialize R2 to 0                                --> comment
+    @R2   // A <- Address[R2]                        --> A-instruction with comment
+    M=0   // R2=0                                    --> C-instruction with comment
+                                                  // --> empty
+(LOOP)                                            // --> L-instruction
+    // Initilize D to R0.                            --> comment
+    // We'll use A for the possible jump location.   --> comment
+    @R0   // A <- Address[R0]                        --> A-instruction with comment
+    D=M   // D=R0                                    --> C-instruction with comment
+    // If R0=0, then we are done multiplying         --> comment
+    @END  // A <- END to prepare for possible jump   --> A-instruction with comment
+    D;JEQ // If D=0, then jump to END                --> C-instruction with comment
+    // Otherwise, add R1 to R2 once                  --> comment
+    @R1   // A <- Address[R1]                        --> A-instruction with comment
+    D=M   // D=R1                                    --> C-instruction with comment
+    @R2   // A <- Address[R2]                        --> A-instruction with comment
+    M=D+M // R2=R1+R2                                --> C-instruction with comment
+    // Decrement R0 by 1                             --> comment
+    @R0   // A <- Address[R0]                        --> A-instruction with comment
+    M=M-1 // R0=R0-1                                 --> C-instruction with comment
+    // Continue looping                              --> comment
+    @LOOP // A <- LOOP                               --> A-instruction with comment
+    0;JMP // Jump to LOOP                            --> C-instruction with comment
+                                                  // --> empty
+(END)                                             // --> L-instruction
+    @END                                          // --> A-instruction
+    0;JMP                                         // --> C-instruction
