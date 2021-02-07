70b7b1b36b2fd68f188a88ce894c3a9a031eb189
bmitc <65685447+bmitc@users.noreply.github.com>
2021-02-07 00:16:00 -0600

    Initial model of virtual machine
    Only includes type definitions for arithmetic, logic, and memory access commands.

---
+++ dotnet/Nand2Tetris/VirtualMachine/Core.fs
@@ -0,0 +1,40 @@
+﻿module VirtualMachine.Core
+
+type Data = Data of int16
+
+type ArithmeticLogicCommand =
+    | Add
+    | Subtract
+    | Negate
+    | Equal
+    | GreaterThan
+    | LessThan
+    | And
+    | Or
+    | Not
+
+type Segment =
+    | Argument
+    | Local
+    | Static
+    | Constant
+    | This
+    | That
+    | Pointer
+    | Temporary
+
+type MemoryAccessCommand =
+    | Push of Segment * index: int
+    | Pop of Segment * index: int
+
+type Stack = Stack of Data list
+
+exception EmptyStack of message: string
+
+let push element (Stack data) =
+    Stack (element :: data)
+
+let pop (Stack data) =
+    match data with
+    | top :: rest -> (top, rest)
+    | []           -> raise (EmptyStack "The stack is empty.")
